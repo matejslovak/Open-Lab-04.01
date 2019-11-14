5635fca2b87f7273facc4dde63d127abb39e15dd
matejslovak <joggurtik1@gmail.com>
2019-11-14 09:13:21 +0100

    done

---
+++ Open-Lab-04.01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Open_Lab_04._01
+{
+    class Program
+    {
+        public static bool DoubleLetters(string str)
+        {
+            for (var i = 0; i < str.Length -1; i++)
+            {
+                if (str[i] == str[i + 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DoubleLetters("woooooooooooooooow"));
+        }
+    }
+}
